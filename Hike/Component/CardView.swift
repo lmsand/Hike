301ff882399898ec58edc7b582da8bca8af65d73
lmsand <lmsand99@icloud.com>
2023-11-10 22:45:19 -0600

    Card component created

---
+++ Hike/Component/CardView.swift
@@ -0,0 +1,42 @@
+//
+//  CardView.swift
+//  Hike
+//
+//  Created by lexi sanders on 11/10/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        // MARK: Card
+        ZStack {
+            CustomBackgroundView()
+            
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            colors: [
+                                Color("ColorIndigoMedium"),
+                                Color("ColorSalmonLight")
+                            ],
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing)
+                    )
+                    .frame(width: 256, height: 256)
+                
+                
+                Image("image-1")
+                    .resizable()
+                .scaledToFit()
+            }
+        }
+        // CARD
+        .frame(width: 320, height: 570)
+    }
+}
+
+#Preview {
+    CardView()
+}
