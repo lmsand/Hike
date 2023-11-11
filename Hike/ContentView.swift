649605b07477dbad260ed43ea3cfcbcae1801249
lmsand <lmsand99@icloud.com>
2023-11-10 22:20:07 -0600

    Initial Commit

---
+++ Hike/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by lexi sanders on 11/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("image-1")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by lexi sanders on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
