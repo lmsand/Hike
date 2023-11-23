e8e5dae91e0b41cddde0426e8c98d3ccd8a0eb99
lmsand <lmsand99@icloud.com>
2023-11-22 18:58:19 -0600

    Motion animation

---
+++ Hike/Component/CustomCircleView.swift
@@ -0,0 +1,45 @@
+//
+//  CustomCircleView.swift
+//  Hike
+//
+//  Created by lexi sanders on 11/19/23.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+    
+    @State private var isAnimateGradient: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(
+                        colors: [
+                            .customIndigoMedium,
+                            .customSalmonLight
+                        ],
+                        startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
+                        endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing)
+                )
+                .onAppear {
+                    withAnimation(.linear(duration: 3.0).repeatForever(autoreverses: true)) {
+                        isAnimateGradient.toggle()
+                    }
+                }
+            MotionAnimationView()
+        } // ZSTACK
+        .frame(width: 256, height: 256)
+        .mask(Circle())
+        .drawingGroup()
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+        .background {
+            Circle()
+                .fill(.teal)
+        }
+}
